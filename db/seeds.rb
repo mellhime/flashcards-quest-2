4df87f5da555294363cd7b00950b835b0c084028
mell_hime <mell_hime@mail.ru>
2017-11-08 23:08:08 +0300

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,5 +10,5 @@ doc = Nokogiri::HTML(open('http://www.learnathome.ru/blog/100-beautiful-words'))
 doc.search('//table/tbody/tr').each do |row|
   original = row.search('td[2]/p')[0].content.downcase
   translated = row.search('td[1]/p')[0].content.downcase
-  Card.create(original_text: original, translated_text: translated, review_date: Date.today, block_id: pack.id, user_id: user.id)
+  Card.create(original_text: original, translated_text: translated, review_date: Date.today, block_id: block.id, user_id: user.id)
 end
