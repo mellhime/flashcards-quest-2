60581155994f23b6e1b3f65c07d3d5fa59648fbd
mell_hime <mell_hime@mail.ru>
2017-11-08 23:15:32 +0300

    fix

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -11,7 +11,7 @@ class ApplicationPolicy
   end
 
   def show?
-    scope.where(:id => record.id).exists?
+    scope.where(id: record.id).exists?
   end
 
   def create?
@@ -53,26 +53,26 @@ class ApplicationPolicy
 
   def rails_admin?(action)
     case action
-      when :dashboard
-        user.has_role?(:admin)
-      when :index
-        user.has_role?(:admin)
-      when :show
-        user.has_role?(:admin)
-      when :new
-        user.has_role?(:admin)
-      when :edit
-        user.has_role?(:admin)
-      when :destroy
-        user.has_role?(:admin)
-      when :export
-        user.has_role?(:admin)
-      when :history
-        user.has_role?(:admin)
-      when :show_in_app
-        user.has_role?(:admin)
-      else
-        raise ::Pundit::NotDefinedError, "unable to find policy #{action} for #{record}."
+    when :dashboard
+      user.has_role?(:admin)
+    when :index
+      user.has_role?(:admin)
+    when :show
+      user.has_role?(:admin)
+    when :new
+      user.has_role?(:admin)
+    when :edit
+      user.has_role?(:admin)
+    when :destroy
+      user.has_role?(:admin)
+    when :export
+      user.has_role?(:admin)
+    when :history
+      user.has_role?(:admin)
+    when :show_in_app
+      user.has_role?(:admin)
+    else
+      raise ::Pundit::NotDefinedError, "unable to find policy #{action} for #{record}."
     end
   end
 end
